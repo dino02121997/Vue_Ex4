ba02e180fd29054d2d59920c7ccff42a293c443d
TA TY TY <tytt@TAs-MacBook-Pro.local>
2023-06-06 14:36:04 +0700

    emit

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea v-model="text" name="" id="" cols="30" rows="10" class="form-input" />
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text = ref('')
+const emit = defineEmits(['submit', 'save'])
+function save() {
+  const postId = 'ggqq' + Math.random()
+  const post = {
+    id: postId,
+    text: text.value,
+    publishedAt: Math.floor(Date.now() / 1000),
+    userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+  }
+  emit('save', { post }) // access under eventData.post
+
+  text.value = ''
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/PageThreadShow.vue
+++ src/views/PageThreadShow.vue
@@ -1,61 +1,56 @@
 <template>
   <div v-if="thread" class="col-large push-top">
     <h1>{{ thread.title }}</h1>
-    <!-- <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-
-          <a href="#">
-            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="" />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
+    <PostList :posts="threadPosts" />
+    <!-- <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input" />
         </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{ postById(postId).text }}
-            </p>
-          </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
         </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
+      </form>
     </div> -->
+     <PostEditor @save="addPost"/>
   </div>
-  <PostList :posts="threadPosts()" />
 </template>
 
 <script setup>
 import sourceData from '@/data.json'
 import PostList from '../components/PostList.vue'
+import PostEditor from '../components/PostEditor.vue'
 import { ref, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 const threads = ref(sourceData.threads)
 const posts = ref(sourceData.posts)
 const users = ref(sourceData.users)
+const newPostText = ref('')
 const props = defineProps(['id'])
 const route = useRoute()
 const thread = computed(() => {
   return threads.value.find((th) => th.id === route.params.id)
 })
-// function postById(postId) {
-//   return posts.value.find((p) => p.id === postId)
-// }
-// function userById(userId) {
-//   return users.value.find((p) => p.id === userId)
-// }
-function threadPosts() {
+const threadPosts = computed(() => {
   return posts.value.filter((post) => post.threadId === route.params.id)
+})
+function addPost(eventData) {
+  // const postId = 'ggqq' + Math.random()
+  const post = {
+    ...eventData.post,
+    threadId: props.id
+  }
+  posts.value.push(post)
+  thread.value.posts.push(post.id)
+  console.log(post)
+  
+  
+  // newPostText.value = ''
 }
 onMounted(() => {
   // Code to be executed when the component is mounted
   console.log('Component mounted!')
-});
+})
 </script>
 
 <style scoped>
